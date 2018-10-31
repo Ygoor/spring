d4582cad15fda1a4150b7c0a69d4e5db00d34108
Polaznik <Polaznik@ISSA-12>
2018-10-31 12:16:34 +0100

    security

---
+++ ordering/src/main/java/net/croz/spring/ordering/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package net.croz.spring.ordering.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http
+//			.csrf().disable()
+//			.cors().disable()	
+			.authorizeRequests()
+				.antMatchers("/styles/**").permitAll()
+				.antMatchers("/proizvod").hasRole("EDIT")			
+				.antMatchers("/dohvatiProizvode").hasAnyRole("EDIT","VIEW")			
+	        	.anyRequest()//allow all urls
+	        	.authenticated()//all URLs are allowed by any authenticated user, no role restrictions.
+			.and()
+			.exceptionHandling()
+				.accessDeniedPage("/denied")
+			.and()	
+			.formLogin()//enable form based authentication
+				.loginPage("/login")//use a custom login URI
+	         	.permitAll(true)//login URI can be accessed by anyone
+	         	.defaultSuccessUrl("/dohvatiProizvode")
+			.and()
+			.logout()//default logout handling
+				.permitAll()//allow all as it will be accessed when user is not logged in anymore
+				.logoutSuccessUrl("http://www.index.hr");//our new logout success url, we are not replacing other defaults.
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("a").password("{noop}a").roles("ADMIN")
+		.and()
+		.withUser("e").password("{noop}e").roles("EDIT")
+		.and()
+		.withUser("v").password("{noop}v").roles("VIEW");
+
+	}
+	
+	
+}
--- ordering/src/main/java/net/croz/spring/ordering/controller/HomeController.java
+++ ordering/src/main/java/net/croz/spring/ordering/controller/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -26,7 +27,7 @@ import net.croz.spring.ordering.repository.ProizvodRepository;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private KupacRepository iKupacRepository;
 	@Autowired
@@ -34,84 +35,97 @@ public class HomeController {
 	@Autowired
 	private ProizvodRepository iProizvodRepository;
 
-	
 	@GetMapping("/welcome")
 	public String helloWorld(Map<String, Object> model) {
-		
+
 		Kupac tKupac = new Kupac();
 		tKupac.setCity("Zagreb");
 		tKupac.setEmail("a@a.com");
 		tKupac.setId(Long.valueOf(1));
 		tKupac.setName("Petar");
 		tKupac.setSurname("Spirek");
-		
+
 		iKupacRepository.save(tKupac);
-		
+
 		model.put("message", "asdasdasd");
 		return "welcome";
 	}
-	
+
 	@GetMapping("/kupac")
 	public String getKupac(Map<String, Object> model) {
-		
+
 		Kupac tKupac = iKupacRepository.getOne(Long.valueOf(1));
-		
+
 		model.put("message", tKupac.getSurname());
 		return "welcome";
 	}
-	
+
 	@GetMapping("/dobavljac")
 	public String setDobavljac(Map<String, Object> model) {
-		
+
 		Dobavljac tDobavljac = new Dobavljac();
 		tDobavljac.setId(Long.valueOf(1));
 		tDobavljac.setName("Dobavljac 1");
-		
+
 		iDobavljacRepository.save(tDobavljac);
-		
+
 		return "welcome";
 	}
-	
-	
-	@GetMapping("/noviProizvod")
+
+	@GetMapping("/proizvod")
 	public String newProizvod(Model model) {
 		model.addAttribute("proizvodForma", new ProizvodForma());
 		model.addAttribute("listaDobavljaca", iDobavljacRepository.findAll());
 		return "proizvodi";
 	}
-	
+
+	@GetMapping(value = { "/proizvod/{id}/", "/proizvod/{id}" })
+	public String getProizvodById(Model model, @PathVariable final String id) {
+
+		ProizvodForma tForma = new ProizvodForma();
+		Proizvod tProizvod = iProizvodRepository.getOne(Long.valueOf(id));
+		tForma.setId(tProizvod.getId());
+		tForma.setName(tProizvod.getName());
+		tForma.setPrice(tProizvod.getPrice());
+		tForma.setSupplier_id(tProizvod.getSupplier_id());
+
+		model.addAttribute("proizvodForma", tForma);
+		model.addAttribute("listaDobavljaca", iDobavljacRepository.findAll());
+		return "proizvodi";
+	}
+
 	@GetMapping("/dohvatiProizvode")
 	public String getProizvod(Model model) {
 		model.addAttribute("listaProizvoda", iProizvodRepository.findAll());
 		return "listaProizvoda";
 	}
-	
+
 	@PostMapping("/spremiProizvod")
-		public String saveProizvod(@Valid @ModelAttribute ProizvodForma forma, BindingResult bindingResult, Model model) {
-        
+	public String saveProizvod(@Valid @ModelAttribute ProizvodForma forma, BindingResult bindingResult, Model model) {
+
 		if (!bindingResult.hasErrors()) {
 			Proizvod tProizvod = new Proizvod();
-			
-//			tProizvod.setId(forma.getId());
+
+			tProizvod.setId(forma.getId());
 			tProizvod.setName(forma.getName());
 			tProizvod.setPrice(forma.getPrice());
 			tProizvod.setSupplier_id(forma.getSupplier_id());
-			
+
 			iProizvodRepository.save(tProizvod);
 			return "redirect:/dohvatiProizvode";
 
-        } else {
-    		model.addAttribute("listaDobavljaca", iDobavljacRepository.findAll());
-    		return "proizvodi";
+		} else {
+			model.addAttribute("listaDobavljaca", iDobavljacRepository.findAll());
+			return "proizvodi";
 
-        }
+		}
 
 	}
-	
+
 	@DeleteMapping("/delete")
 	public ResponseEntity deteleProizvod(@RequestBody String id) {
-		if (id != null)  {
-			iProizvodRepository.deleteById(Long.valueOf(id));	
+		if (id != null) {
+			iProizvodRepository.deleteById(Long.valueOf(id));
 		}
 		return new ResponseEntity(HttpStatus.OK);
 	}
